c13a36088a180141ce5c88899d5e76f3b9bb7bfa
Bojan Zivanovic <bojanz@gmail.com>
2023-05-17 22:48:18 +0200

    Add subdivisions for Papua New Guinea (PG), Seychelles (SC), United States Minor Outlying Islands (UM).
    (cherry picked from commit 50f84c13b01866b92a2ae77034cd2608ccb79a53)

--- src/AddressFormat/AddressFormatRepository.php
+++ src/AddressFormat/AddressFormatRepository.php
@@ -1165,6 +1165,7 @@ class AddressFormatRepository implements AddressFormatRepositoryInterface
                     'addressLine1', 'locality', 'administrativeArea',
                 ],
                 'postal_code_pattern' => '\d{3}',
+                'subdivision_depth' => 1,
             ],
             'PH' => [
                 'format' => "%givenName %familyName\n%organization\n%addressLine1\n%addressLine2\n%addressLine3\n%dependentLocality, %locality\n%postalCode %administrativeArea",
@@ -1293,6 +1294,7 @@ class AddressFormatRepository implements AddressFormatRepositoryInterface
                     'administrativeArea',
                 ],
                 'administrative_area_type' => 'island',
+                'subdivision_depth' => 1,
             ],
             'SD' => [
                 'format' => "%givenName %familyName\n%organization\n%addressLine1\n%addressLine2\n%addressLine3\n%locality\n%postalCode",
@@ -1499,6 +1501,7 @@ class AddressFormatRepository implements AddressFormatRepositoryInterface
                 'administrative_area_type' => 'state',
                 'postal_code_type' => 'zip',
                 'postal_code_pattern' => '96898',
+                'subdivision_depth' => 1,
             ],
             'US' => [
                 'format' => "%givenName %familyName\n%organization\n%addressLine1\n%addressLine2\n%addressLine3\n%locality, %administrativeArea %postalCode",
